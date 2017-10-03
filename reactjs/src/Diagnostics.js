ad0d8e38fe2f15244b0ac918c9c0cfd6598172e5
Steven Hadcroft <steven2007@stevenhadcroft.com>
2017-10-03 21:21:46 +0100

    custom environment variable tweak

--- reactjs/src/Diagnostics.js
+++ reactjs/src/Diagnostics.js
@@ -13,8 +13,8 @@ class Diagnostics extends Component {
         return (
             <div style={diagStyle}>
                 <br/>You are running this application in {process.env.NODE_ENV}
-                <br/>You are running this application in {process.env.REACT_APP_VAR_1}
-                <br/>You are running this application in {process.env.REACT_APP_VAR_2}
+                <br/>Custom environment variable 1 : {process.env.REACT_APP_VAR_1}
+                <br/>Custom environment variable 2 :  {process.env.REACT_APP_VAR_2}
             </div>
         );
     }
